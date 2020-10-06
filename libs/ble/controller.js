312833d11181a4a6a47a45c179611c8074be0fdd
Lucas Bandin <lucas.bandin.arcida@gmail.com>
2020-10-06 16:32:36 -0400

    BLE

--- libs/ble/controller.js
+++ libs/ble/controller.js
@@ -3,7 +3,24 @@ const receiveCharUuid = '12345678-1234-5678-1234-56789abcdef1';
 const sendCharUuid = '12345678-1234-5678-1234-56789abcdef3';
 let device, sendCharacteristic, receiveCharacteristic;
 
-bleConnectButton.onclick = async () => {
+
+// Show BLE output
+function addBleOutput(txt) {
+  const node = document.createElement("DIV");
+  const textNode = document.createTextNode(txt);
+  node.appendChild(textNode);
+  bleOutputArea.appendChild(node);
+}
+
+// Show BLE response
+function addBleResult(txt) {
+  const node = document.createElement("DIV");
+  const textNode = document.createTextNode(txt);
+  node.appendChild(textNode);
+  bleResultArea.appendChild(node);
+}
+
+/*bleConnectButton.onclick = async () => {
   device = await navigator.bluetooth
     .requestDevice({
       filters: [{
@@ -17,6 +34,22 @@ bleConnectButton.onclick = async () => {
     await service.getCharacteristic(receiveCharUuid);
   sendCharacteristic =
     await service.getCharacteristic(sendCharUuid);
+
+  device.ongattserverdisconnected = disconnect;
+  bleStatus.innerText = 'Connected';
+};*/
+
+
+bleConnectButton.onclick = () => {
+  navigator.bluetooth.requestDevice({filters: [{services: [primaryServiceUuid]}]})
+    .then(device => {
+      addBleResult(`> Name: ${device.name}`);
+      addBleResult(`> Id: ${device.id}`);
+      addBleResult(`> Connected: ${device.gatt.connected}`);
+    })
+    .catch(error => {
+      addBleResult(`Argh! ${error}`);
+    });
 
   device.ongattserverdisconnected = disconnect;
   bleStatus.innerText = 'Connected';
