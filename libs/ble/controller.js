bae1bea911a05735d4ac7c5fcee00826d59eeab1
Lucas Bandin <lucas.bandin.arcida@gmail.com>
2020-10-06 16:21:34 -0400

    BLE III

--- libs/ble/controller.js
+++ libs/ble/controller.js
@@ -1,4 +1,4 @@
-const primaryServiceUuid = '12345678-1234-5678-1234-56789abcdef0';
+const primaryServiceUuid = '0000ffe0-0000-1000-8000-00805f9b34fb';
 const receiveCharUuid = '12345678-1234-5678-1234-56789abcdef1';
 const sendCharUuid = '12345678-1234-5678-1234-56789abcdef3';
 let device, sendCharacteristic, receiveCharacteristic;
@@ -6,7 +6,9 @@ let device, sendCharacteristic, receiveCharacteristic;
 bleConnectButton.onclick = async () => {
   device = await navigator.bluetooth
     .requestDevice({
-      filters: []
+      filters: [{
+        services: [primaryServiceUuid]
+      }]
     });
   const server = await device.gatt.connect();
   const service =
